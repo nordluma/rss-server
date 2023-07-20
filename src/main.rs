4a90384cf38f95726e8a2a826e9c9f708d7dd99b
nordluma <maxnordlund.95@hotmail.com>
2023-07-21 00:06:15 +0300

    feat: connect to postgres db

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,81 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder, Result};
-use serde::Deserialize;
+use std::net::TcpListener;
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Responder, Result};
+use secrecy::{ExposeSecret, Secret};
+use serde::Deserialize;
+use serde_aux::field_attributes::deserialize_number_from_string;
+use sqlx::postgres::{PgConnectOptions, PgPoolOptions, PgSslMode};
+use sqlx::PgPool;
+
+const ADDR: &str = "127.0.0.1";
+
+#[derive(Deserialize)]
+struct DatabaseSettings {
+    username: String,
+    password: Secret<String>,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
+    port: u16,
+    host: String,
+    database_name: String,
+    require_ssl: bool,
+}
+
+impl DatabaseSettings {
+    fn new() -> DatabaseSettings {
+        // use dummy data now, will switch to a config file later
+        DatabaseSettings {
+            username: "test".to_string(),
+            password: "test".to_string().into(),
+            port: 5432,
+            host: "postgres".to_string(),
+            database_name: "test".to_string(),
+            require_ssl: true,
+        }
+    }
+    fn connect_db(&self) -> PgConnectOptions {
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::Require
+        } else {
+            PgSslMode::Prefer
+        };
+
+        PgConnectOptions::new()
+            .host(&self.host)
+            .username(&self.username)
+            .password(self.password.expose_secret())
+            .port(self.port)
+            .database(&self.database_name)
+            .ssl_mode(ssl_mode)
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let db_configs = DatabaseSettings::new();
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(db_configs.connect_db());
+
+    let listener = TcpListener::bind(ADDR)?;
+
+    run(listener, connection_pool)?.await?;
+
+    Ok(())
+}
+
+fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    let db_pool = web::Data::new(db_pool);
+
+    let server = HttpServer::new(move || {
         App::new()
             .configure(users)
             .route("/healthcheck", web::get().to(health_check))
+            .app_data(db_pool.clone())
     })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    .listen(listener)?
+    .run();
+
+    Ok(server)
 }
 
 async fn health_check() -> HttpResponse {
@@ -25,12 +90,22 @@ fn users(cfg: &mut web::ServiceConfig) {
     );
 }
 
+// This is still for testing, the real struct will be defined later
 #[derive(Deserialize)]
 struct User {
     name: String,
 }
 
 async fn create_user(user: web::Json<User>) -> Result<String> {
+    /*
+     * sqlx::query!(
+     *      r#"
+     *          INSERT INTO user (id, created_at, updated_at, name)
+     *          VALUES ($1, $2, $3, $4)
+     *      "
+     * )
+     */
+
     Ok(format!("Creating user: {}", user.name))
 }
 
