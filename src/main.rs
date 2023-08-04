177321a9aaac14455b0d89b44565f81e954008bd
nordluma <maxnordlund.95@hotmail.com>
2023-08-04 14:33:12 +0300

    refactor(store): move database logic to store module

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,27 @@
+use std::env;
 use std::net::TcpListener;
 
 use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Result};
-use secrecy::{ExposeSecret, Secret};
-use serde::Deserialize;
-use serde_aux::field_attributes::deserialize_number_from_string;
-use sqlx::postgres::{PgConnectOptions, PgPoolOptions, PgSslMode};
-use sqlx::PgPool;
+use store::Store;
 
 mod routes;
+mod store;
 
 const ADDR: &str = "127.0.0.1";
 
-#[derive(Deserialize)]
-struct DatabaseSettings {
-    username: String,
-    password: Secret<String>,
-    #[serde(deserialize_with = "deserialize_number_from_string")]
-    port: u16,
-    host: String,
-    database_name: String,
-    require_ssl: bool,
-}
-
-impl DatabaseSettings {
-    fn new() -> DatabaseSettings {
-        // use dummy data now, will switch to a config file later
-        DatabaseSettings {
-            username: "test".to_string(),
-            password: "test".to_string().into(),
-            port: 5432,
-            host: "postgres".to_string(),
-            database_name: "test".to_string(),
-            require_ssl: true,
-        }
-    }
-    fn connect_db(&self) -> PgConnectOptions {
-        let ssl_mode = if self.require_ssl {
-            PgSslMode::Require
-        } else {
-            PgSslMode::Prefer
-        };
-
-        PgConnectOptions::new()
-            .host(&self.host)
-            .username(&self.username)
-            .password(self.password.expose_secret())
-            .port(self.port)
-            .database(&self.database_name)
-            .ssl_mode(ssl_mode)
-    }
-}
-
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let db_configs = DatabaseSettings::new();
-    let connection_pool = PgPoolOptions::new()
-        .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy_with(db_configs.connect_db());
+    let store = Store::new(env::var("DATABASE_URL").unwrap().as_str()).await;
 
     let listener = TcpListener::bind(ADDR)?;
 
-    run(listener, connection_pool)?.await?;
+    run(listener, store.unwrap())?.await?;
 
     Ok(())
 }
 
-fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
-    let db_pool = web::Data::new(db_pool);
+fn run(listener: TcpListener, store: Store) -> Result<Server, std::io::Error> {
+    let db_pool = web::Data::new(store);
 
     let server = HttpServer::new(move || {
         App::new()
--- src/store.rs
+++ src/store.rs
@@ -1,4 +1,43 @@
-#[derive(Debug)]
+use sqlx::{postgres::PgPoolOptions, PgPool};
+
+use crate::routes::authentication::Account;
+
+#[derive(Debug, Clone)]
 pub struct Store {
-    connection: PgPool,
+    pub connection: PgPool,
+}
+
+impl Store {
+    pub async fn new(db_url: &str) -> Result<Self, sqlx::Error> {
+        let db_pool = match PgPoolOptions::new()
+            .max_connections(5)
+            .connect(db_url)
+            .await
+        {
+            Ok(pool) => pool,
+            Err(e) => panic!("Couldn't establish DB connection: {}", e),
+        };
+
+        Ok(Store {
+            connection: db_pool,
+        })
+    }
+
+    pub async fn create_user(self, account: Account) -> Result<bool, sqlx::Error> {
+        match sqlx::query(
+            "INSERT INTO users (id, created_at, updated_at, name, api_key)
+            VALUES ($1, $2, $3, $4, encode(random()::text:bytea), 'hex')
+            RETURNING *",
+        )
+        .bind(account.id)
+        .bind(account.created_at)
+        .bind(account.updated_at)
+        .bind(account.name)
+        .fetch_one(&self.connection)
+        .await
+        {
+            Ok(_) => Ok(true),
+            Err(e) => Err(e),
+        }
+    }
 }
