57d8fbcbc4ba217e513c430c4d4d4900710b6c89
nordluma <maxnordlund.95@hotmail.com>
2023-08-22 22:44:21 +0300

    feat(authentication): handler for getting user by api key

--- src/routes/authentication.rs
+++ src/routes/authentication.rs
@@ -12,16 +12,25 @@ pub struct NewAccount {
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
-pub struct Account {
+pub struct CreateAccount {
     pub id: Uuid,
     pub created_at: NaiveDateTime,
     pub updated_at: NaiveDateTime,
     pub name: String,
 }
 
-impl Account {
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct Account {
+    pub id: Uuid,
+    pub created_at: NaiveDateTime,
+    pub updated_at: NaiveDateTime,
+    pub name: String,
+    pub api_key: String,
+}
+
+impl CreateAccount {
     fn new(name: &str) -> Self {
-        Account {
+        CreateAccount {
             id: Uuid::new_v4(),
             created_at: Utc::now().naive_utc(),
             updated_at: Utc::now().naive_utc(),
@@ -32,9 +41,24 @@ impl Account {
 
 pub async fn register(user: web::Json<NewAccount>, store: web::Data<Store>) -> HttpResponse {
     let store = store.get_ref().to_owned();
-    let account = Account::new(&user.name);
+    let account = CreateAccount::new(&user.name);
+
     match Store::insert_user(store, account).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::BadRequest().finish(),
+    }
+}
+
+pub async fn get_user_by_api_key(api_key: &str, store: web::Data<Store>) -> HttpResponse {
+    let store = store.get_ref().to_owned();
+
+    let user = match Store::get_user_by_api_key(store, api_key).await {
+        Ok(user) => user,
+        Err(_) => return HttpResponse::InternalServerError().finish(),
+    };
+
+    match user {
+        Some(user) => HttpResponse::Ok().json(user),
+        None => HttpResponse::Unauthorized().finish(),
     }
 }
