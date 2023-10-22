cb0f3a3ed2c96e60b94831c69a3f19d56f56a270
nordluma <maxnordlund.95@hotmail.com>
2023-10-22 21:20:52 +0300

    feat(feed): create newtype for extracting form data from request

--- src/routes/feed.rs
+++ src/routes/feed.rs
@@ -1,19 +1,40 @@
 use actix_web::{web, HttpResponse};
-use chrono::NaiveDateTime;
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 use crate::store::Store;
 
-#[derive(Debug, Deserialize, Serialize)]
-pub struct Feed {
-    pub id: Uuid,
-    pub created_at: NaiveDateTime,
-    pub updated_at: NaiveDateTime,
+#[derive(Debug, Deserialize)]
+pub struct NewFeed {
     pub name: String,
     pub url: String,
     pub user_id: Uuid,
-    pub last_fetched_at: Option<NaiveDateTime>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Feed {
+    pub id: Uuid,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+    pub name: String,
+    pub url: String,
+    pub user_id: Uuid,
+    pub last_fetched_at: Option<DateTime<Utc>>,
+}
+
+impl From<NewFeed> for Feed {
+    fn from(value: NewFeed) -> Self {
+        Self {
+            id: Uuid::new_v4(),
+            created_at: Utc::now(),
+            updated_at: Utc::now(),
+            name: value.name,
+            url: value.url,
+            user_id: value.user_id,
+            last_fetched_at: None,
+        }
+    }
 }
 
 pub fn feed(cfg: &mut web::ServiceConfig) {
@@ -32,7 +53,7 @@ pub async fn get_feeds(store: web::Data<Store>) -> HttpResponse {
     }
 }
 
-pub async fn create_feed(store: web::Data<Store>) -> HttpResponse {
+pub async fn create_feed(store: web::Data<Store>, _feed: web::Form<NewFeed>) -> HttpResponse {
     let store = store.get_ref().to_owned();
     match store.insert_feed().await {
         Ok(_) => HttpResponse::Created().finish(),
